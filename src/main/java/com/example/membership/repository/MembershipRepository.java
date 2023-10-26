64fd495249293134ac8eaa10cd0169c084e6def9
myway00 <junior0614@naver.com>
2023-10-26 14:16:25 +0900

    #3 ADD : 멤버십 타입을 등록했으면 중복 검사 TEST

--- src/main/java/com/example/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/membership/repository/MembershipRepository.java
@@ -1,10 +1,13 @@
 package com.example.membership.repository;
 
 import com.example.membership.entity.Membership;
+import com.example.membership.entity.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Repository 타입의 빈을 등록하기 위해서는 @Repository 어노테이션을 붙여주어야 한다.
 // 그러나, JpaRepository 하위에 @Repository가 이미 붙어있으므로 @Repository를 붙여주지 않아도 된다.
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+
+    Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
 
 }

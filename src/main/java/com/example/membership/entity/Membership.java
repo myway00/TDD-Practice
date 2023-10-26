b2c90e0390ca7f53bf306f7dfe2ae33cd3c25518
myway00 <junior0614@naver.com>
2023-10-26 13:55:16 +0900

    #3 ADD : 멤버십 등록 API TEST

--- src/main/java/com/example/membership/entity/Membership.java
+++ src/main/java/com/example/membership/entity/Membership.java
@@ -1,16 +1,43 @@
 package com.example.membership.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
-@NoArgsConstructor // JPA에서 관리되는 엔티티를 위해서는 기본 생성자가 필요
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Membership {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 20)
+    private String membershipName;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer point;
+
+    @CreationTimestamp
+    @Column(nullable = false, length = 20, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = true, length = 20)
+    private LocalDateTime updatedAt;
 }
--- src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/example/membership/repository/MembershipRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.example.membership.repository;
 
+import com.example.membership.entity.Membership;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 // @DataJpaTest: JPA Repository들에 대한 빈들을 등록하여 단위 테스트의 작성을 용이하게 함
@@ -17,8 +18,22 @@ public class MembershipRepositoryTest {
     private MembershipRepository membershipRepository;
 
     @Test
-    public void MembershipRepository가Null이아님() {
-        assertThat(membershipRepository).isNotNull();
+    public void 멤버십등록() {
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
+
+        // when
+        final Membership result = membershipRepository.save(membership);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("userId");
+        assertThat(result.getMembershipName()).isEqualTo("네이버");
+        assertThat(result.getPoint()).isEqualTo(10000);
     }
 
 }
